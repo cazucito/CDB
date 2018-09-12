a7608b54f714d3557042933417d96ef3e9e7f3f4
180910_GPO_SALINAS <180910_GPO_SALINAS@LAP_57>
2018-09-12 14:38:22 -0500

    SQLException

--- CDBalpha/src/com/gs/cdb/model/ClienteDAO.java
+++ CDBalpha/src/com/gs/cdb/model/ClienteDAO.java
@@ -50,12 +50,12 @@ public class ClienteDAO {
         }
         return clientes;
     }
-    
+
     public Cliente obtenCliente(int id) {
         Optional<Cliente> cl = obtenListaClientesMemoria().stream()
-                .filter(c->c.getIdCliente()==id).findFirst();
+                .filter(c -> c.getIdCliente() == id).findFirst();
         Cliente clienteEncontrado = null;
-        if(cl.isPresent()) {
+        if (cl.isPresent()) {
             clienteEncontrado = cl.get();
         }
         return clienteEncontrado;
@@ -64,16 +64,26 @@ public class ClienteDAO {
     private Connection obtenConexion() {
         try {
             cxn = DriverManager.getConnection(url, usuario, password);
-        } catch (Exception ex) {
-            System.err.println("CXN ERROR" + ex.getMessage());
+        } catch (SQLException ex) {
+            while (ex != null) {
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("Error Code:" + ex.getErrorCode());
+                System.out.println("Message: " + ex.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause:" + t);
+                    t = t.getCause();
+                }
+                ex = ex.getNextException();
+            }
         }
         return cxn;
     }
-    
+
     private List<Cliente> obtenListaClientesMemoria() {
         List<Cliente> clientes = new ArrayList<Cliente>();
         clientes.add(
-         new Cliente(
+                new Cliente(
                         10,
                         "Nombre1",
                         "Paterno1",
@@ -85,7 +95,7 @@ public class ClienteDAO {
                 )
         );
         clientes.add(
-         new Cliente(
+                new Cliente(
                         20,
                         "Nombre2",
                         "Paterno2",
