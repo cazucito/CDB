931efd4f3b9c5a22cd19c2d7f82b834198facda2
180910_GPO_SALINAS <180910_GPO_SALINAS@LAP_57>
2018-09-12 14:36:13 -0500

    obtenCliente

--- CDBalpha/src/com/gs/cdb/model/ClienteDAO.java
+++ CDBalpha/src/com/gs/cdb/model/ClienteDAO.java
@@ -7,8 +7,10 @@ import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -48,6 +50,16 @@ public class ClienteDAO {
         }
         return clientes;
     }
+    
+    public Cliente obtenCliente(int id) {
+        Optional<Cliente> cl = obtenListaClientesMemoria().stream()
+                .filter(c->c.getIdCliente()==id).findFirst();
+        Cliente clienteEncontrado = null;
+        if(cl.isPresent()) {
+            clienteEncontrado = cl.get();
+        }
+        return clienteEncontrado;
+    }
 
     private Connection obtenConexion() {
         try {
@@ -56,5 +68,34 @@ public class ClienteDAO {
             System.err.println("CXN ERROR" + ex.getMessage());
         }
         return cxn;
+    }
+    
+    private List<Cliente> obtenListaClientesMemoria() {
+        List<Cliente> clientes = new ArrayList<Cliente>();
+        clientes.add(
+         new Cliente(
+                        10,
+                        "Nombre1",
+                        "Paterno1",
+                        "Materno1",
+                        "RFC1",
+                        LocalDate.of(2000, Month.MARCH, 15),
+                        10,
+                        10
+                )
+        );
+        clientes.add(
+         new Cliente(
+                        20,
+                        "Nombre2",
+                        "Paterno2",
+                        "Materno2",
+                        "RFC2",
+                        LocalDate.of(2000, Month.MARCH, 15),
+                        20,
+                        20
+                )
+        );
+        return clientes;
     }
 }
